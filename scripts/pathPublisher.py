a1b162018f1e102cf04bbd14d020a3b4ce4e57fb
david-alejo <churr.te@gmail.com>
2022-02-24 15:13:37 +0100

    Added parameter service pathpub

--- scripts/pathPublisher.py
+++ scripts/pathPublisher.py
@@ -10,30 +10,32 @@ from nav_msgs.msg import Path
 
 if __name__ == '__main__':
     
-    path_publisher = rospy.Publisher('/publish_path', Path, queue_size=10)
+    path_publisher = rospy.Publisher('/path', Path, queue_size=10)
     rospy.init_node('path_publisher', anonymous=False)  
     seq = 0
+    goals = sorted(rospy.get_param('/path').items())
     
     while not rospy.is_shutdown():
         path = Path()
         path.header.frame_id = rospy.get_param("~frame", default="map")
         path.header.stamp = rospy.Time.now()
         path.header.seq = seq
-        arg = raw_input('Escribe un nuevo path. Ejemplo "2,-1;1,-2": \n')
-        try:
-            for punto in arg.split(";"):
-                x = float(punto.split(",")[0])
-                y = float(punto.split(",")[1])
-                
-                pose = PoseStamped()
-                pose.pose.position.x = x
-                pose.pose.position.y = y
-                pose.pose.position.z = 0
+        seq_goals = 0
+        for i in goals:
+            pose = PoseStamped()
+            pose.header.frame_id = path.header.frame_id
+            pose.header.seq = seq_goals
+            seq_goals += 1
+            pose.pose.position.x = i[1]['x']
+            pose.pose.position.y = i[1]['y']
+            pose.pose.position.z = 0
             
-                path.poses.append(pose)
-            path_publisher.publish(path)
-            rospy.loginfo("Path enviado!\n")
+            path.poses.append(pose)
+            rospy.loginfo("Added point %f, %f", pose.pose.position.x, pose.pose.position.y)
+        path_publisher.publish(path)
+        rospy.loginfo("Path enviado!\n")
             
-            seq = seq + 1
-        except:
-            rospy.loginfo("Error de formato. Recuerda que debe seguir el formato: 'x1,y1;x2,y2'\n")
+        seq = seq + 1
+
+        for i in range(50):
+            rospy.sleep(1)
